1574e0b544c76b60427d319571c05b34ffa03881
Dragos Romaniuc <dragos.romaniuc@3pillarglobal.com>
2020-09-28 03:16:37 +0300

    build

---
+++ build/precache-manifest.58b68b3cb319a47b3900fba34a2e3811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d32772d07e56bed1d03d9d1c620fe54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff8c12ed78ebf754b15",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "659d1087b5632e1e8a17",
+    "url": "/static/js/2.d688d3a4.chunk.js"
+  },
+  {
+    "revision": "31454aec93bfb5cc278478a56cde4a37",
+    "url": "/static/js/2.d688d3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff8c12ed78ebf754b15",
+    "url": "/static/js/main.f3cf7f0f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "d1bc8b1957dc1ec95e1e2d39beed5da0",
+    "url": "/static/media/trading.d1bc8b19.jpg"
+  }
+]);
